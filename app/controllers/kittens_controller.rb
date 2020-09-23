23908b2e6733e075dce617b3a94c0e0a469997b7
BraianStrak <braianstrak1999@gmail.com>
2020-09-23 22:33:03 +0100

    KittensController

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,29 @@
+class KittensController < ApplicationController
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy 
+
+    end
+end
