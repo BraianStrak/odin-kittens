54a1bff747979197f0f3261f74f7071cdfc4f83c
BraianStrak <braianstrak1999@gmail.com>
2020-09-23 22:50:05 +0100

    Added functionality to kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,18 +1,24 @@
 class KittensController < ApplicationController
     def index
-
+        @kittens = Kitten.all
     end
 
     def create
+        @kitten = kittens.build(kitten_params)
 
+        if @kitten.save
+            redirect_to root_path
+        else 
+            render :new
+        end
     end
 
     def show
-
+        
     end
 
     def new
-
+        @kitten = Kitten.new
     end
 
     def edit
@@ -25,5 +31,11 @@ class KittensController < ApplicationController
 
     def destroy 
 
+    end
+
+    private
+
+    def kitten_params
+        params.require(:post).permit(:name, :age, :cuteness, :softness)
     end
 end
